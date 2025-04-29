00cec938a1b59d1f9729381e90462d87e610bcaf
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-04-29 14:11:50 +0600

    i have added here navbar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../components/nav/Nav';
 
 const Root = () => {
     return (
         <>
+            <Navbar />
             <Outlet />
         </>
     );
